2f78fadb5d3dc1926199257e8b890f114960c249
canberko560 <canberkvarli@gmail.com>
2021-06-03 14:17:47 -0700

    add rspec for two_sum

--- W4D4/lib/main.rb
+++ W4D4/lib/main.rb
@@ -1,3 +1,21 @@
+
 def my_uniq(arr)
     arr.uniq
+end
+
+
+class Array
+    def two_sum
+
+        result = []
+
+        self.each_with_index do |num,idx|
+            (idx+1..self.length-1).each do |i|
+                if self[idx] + self[i] == 0
+                    result << [idx,i]
+                end
+            end
+        end
+        result
+    end
 end
--- W4D4/spec/main_spec.rb
+++ W4D4/spec/main_spec.rb
@@ -6,3 +6,10 @@ describe "#my_uniq" do
         expect(my_uniq([1,2,1,3,3])).to eq([1,2,3])
     end
 end
+
+
+describe "#two_sum" do
+    it "returns 2D array where elements in the array sum up to 0" do
+        expect([-1,0,2,-2,1].two_sum).to eq([[0,4],[2,3]])
+    end
+end
