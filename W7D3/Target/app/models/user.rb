3b1ae44788de22ae06c30f63880504087f9fffdb
Shirley Tang <shirleytang1996@gmail.com>
2021-06-23 17:39:03 -0400

    test user validations

--- W7D3/Target/app/models/user.rb
+++ W7D3/Target/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :password, length:{minimum:6, allow_nil: true}
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+    def password=(password)
+        @password=password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
--- W7D3/Target/spec/models/user_spec.rb
+++ W7D3/Target/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) {User.create!(username: 'user1', password: 'password')}
+  
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:password)}
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_length_of(:password).is_at_least(6)}
+
+
 end
