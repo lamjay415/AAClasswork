0b9b83f8b4ef75003cd48ceaf167a6542ce5573f
Shirley Tang <shirleytang1996@gmail.com>
2021-06-23 19:28:31 -0400

    fix git

--- W7D3/Target/app/models/user.rb
+++ W7D3/Target/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password, :password_digest, presence: true
     validates :password, length:{minimum:6, allow_nil: true}
     after_initialize :ensure_session_token
 
@@ -12,5 +12,25 @@ class User < ApplicationRecord
 
     def ensure_session_token
         self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def is_Password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_Password?(password)
+            user
+        else
+            nil
+        end
     end
 end
--- W7D3/Target/spec/models/user_spec.rb
+++ W7D3/Target/spec/models/user_spec.rb
@@ -7,6 +7,51 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:password)}
     it {should validate_uniqueness_of(:username)}
     it {should validate_length_of(:password).is_at_least(6)}
+    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:session_token)}
+    it {should validate_uniqueness_of(:session_token)}
 
+    # describe "password setter" do 
 
+      # it "should set up password_digest" do
+      #   expect(user.password='password').to eq(BCrypt::Password.new.isPassword?('password'))
+      # end
+
+    # end
+
+    describe "is_password? " do 
+      let!(:user) {User.create!(username: 'user2', password: 'password')}
+
+      context "valid_password" do
+        it "should return true" do
+          expect(user.is_Password?('password')).to be true
+        end
+      end
+
+      context "invalid_password" do
+        it "should return false" do
+          expect(user.is_Password?('testing')).to be false
+        end
+      end
+
+    end
+
+    describe "reset_session_token!" do
+      # user factory  
+      let!(:user) {FactoryBot.create(:user)}
+      #let!(:user) {User.create!(username: 'user2', password: 'password')}
+      context "reset sucessfully" do
+        it "should generate new token after reset" do
+          old_token = user.session_token
+          expect(user.reset_session_token!).to_not eq(old_token)
+        end
+      end
+      
+      context "reset failed" do
+        it "should not generate new token after reset" do
+          old_token = user.session_token
+          expect(user.session_token).to eq(old_token)
+        end
+      end
+    end
 end
