fe575c59a1ae653b2c19e13e2f8b3b2676f3ab79
Jay Lam <lamjay415@gmail.com>
2021-07-22 14:19:02 -0700

    started rails backend

---
+++ W10D4/todos/app/controllers/api/todos_controller.rb
@@ -0,0 +1,34 @@
+def show
+    render json: Todo.find(params[:id]) 
+end
+
+def index
+    @todos = Todo.all
+    render json: @todos
+end
+
+def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+        render json: @todo
+    else
+        render json: @todo.errors.full_message, status: 422
+    end
+end
+
+def update
+    @todo = Todo.find(params[:id])
+    @todo.update!(todo_params)
+    render json: @todo
+end
+
+def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    @todos = Todo.all
+    render json: @todos
+end
+
+def todo_params
+    params.require(:todo).permit(:title, :body, :done)
+end
