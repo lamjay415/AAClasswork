c325a3f8c42f3d823924fc65076af7d74a2650c8
brassmacks <jadepavedpath@gmail.com>
2021-07-22 17:03:01 -0700

    'built middleware, started thunk'

--- W10D4/todos_project/todos_project/frontend/store/store.js
+++ W10D4/todos_project/todos_project/frontend/store/store.js
@@ -1,6 +1,6 @@
 import rootReducer from '../reducers/root_reducer';
 import {createStore} from 'redux';
-
+import thunk from '../frontend/middleware/thunk'
 
 const configureStore = (preloadedState = {}) => {
     return createStore(rootReducer, preloadedState)
--- W10D4/todos_project/todos_project/frontend/todo_redux.jsx
+++ W10D4/todos_project/todos_project/frontend/todo_redux.jsx
@@ -10,6 +10,7 @@ import { Root } from './components/root'
 import { allTodos } from "./reducers/selectors"
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {fetchTodos} from './util/todo_api_util'
 
 document.addEventListener('DOMContentLoaded', () =>{
   const store = configureStore();
@@ -17,10 +18,10 @@ document.addEventListener('DOMContentLoaded', () =>{
   const newTodos = [{ id: 1, title: 'Learn Redux', body: 'It is fundamental', done: false }, { id: 2, ...etc }, ...etc];
   const newSteps = [{ id: 1, title: 'Dispatch actions', done: false, todo_id: 1 }, { id: 2, ...etc }, ...etc];
   
-  const rootElement = document.getElementById("rootElement")
+  const rootElement = document.getElementById("root")
   ReactDOM.render(<Root store={store} />, rootElement);
   window.allTodos = allTodos;
-
+  window.fetchTodos = fetchTodos;
   window.newTodos = newTodos;
   window.newSteps = newSteps;
   window.store = store;
@@ -29,35 +30,6 @@ document.addEventListener('DOMContentLoaded', () =>{
   window.receive_steps = receive_steps;
   window.receive_step = receive_step;
   window.remove_step = remove_step;
-
-  // console.log('test 1: before ');
-  // console.log(store.getState());
-  store.dispatch(receive_todos(newTodos));
-  store.dispatch(receive_steps(newSteps));
-  // console.log('after ');
-  // console.log(store.getState()); // should return only the new todos and steps
-
-  // console.log('test 2: before ');
-  // console.log(store.getState());
-  store.dispatch(receive_todo({ id: 3, title: "New Todo", ...etc }));
-  store.dispatch(receive_step({ id: 3, title: "New Step", todo_id: 2, done: false, ...etc }));
-  // console.log('after ');
-  // console.log(store.getState()); // should include the newly added todo and step
-
-  // console.log('test 3: before ');
-  // console.log(store.getState());
-  store.dispatch(receive_todo({ id: 3, title: "Newer Todo", ...etc }));
-  store.dispatch(receive_step({ id: 3, title: "Newer Step", done: true, ...etc }));
-  // console.log('after ');
-  // console.log(store.getState()); // should update the previously added todo and step
-
-  // console.log('test 4: before ');
-  // console.log(store.getState());
-  // store.dispatch(remove_todo({ id: 3, title: "Newer Todo", ...etc }));
-  // store.dispatch(remove_step({ id: 3, title: "Newer Step", ...etc }));
-  // console.log('after ');
-  // console.log(store.getState()); // should not include the previously added todo
-
+  
 });
 
-// Test your code - Import the store to your entry file.Create your store by calling configureStore, set window.store = store in your entry file and then call window.store.getState() in your console.Make sure that this function returns the initial application state described above.Don't move on until it does!
